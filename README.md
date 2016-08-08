ea1db126b5bc4c0b30cf104fd76e453164431e2a
cwklausing <cwklausing@gmail.com>
2016-08-08 16:57:49 -0500

    Recursive factorial example added

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 #Learn 4 Semesters of Computer Science in 5 Hours
 ##Walking through the [Front End Masters Course](https://frontendmasters.com/courses/computer-science/)
 
-This course gives an overview of some of the biggest topics in computer science: Algorithms and Big O Analysis, Recursion, Sorting, Data Structures and Functional Programming.
+This course gives an overview of some of the biggest topics in computer science:
+Algorithms and Big O Analysis, Recursion, Sorting, Data Structures and Functional Programming. Also
+allows me to play around with ES6 as well.
--- Recursion.js
+++ Recursion.js
@@ -1,21 +1,39 @@
+/**
+ * Function to write to the DOM
+ * @param msg
+ */
 let wr = (msg='------------') => document.write(`<br>${msg}`);
-
 let functionCalls = 0;
+
+/**
+ * Increment to see how many times recursive function runs
+ */
 function incrementFuncCalls(){
   functionCalls++;
 }
 
+/**
+ * Basic Recursion Function
+ * @param max
+ * @param current
+ */
 function basicRecursion(max, current) {
   if (current > max) return;
   wr(current);
   basicRecursion(max, current+1);
 }
 //Basic recursion print
-wr('Basic Recursion')
+wr('Basic Recursion');
 basicRecursion(5,1);
 wr()
 //Next section
 wr('Fibonacci Sequence');
+
+/**
+ * Fibonacci function using recursion
+ * @param n
+ * @returns {*}
+ */
 function fibonacci(n) {
   incrementFuncCalls();
   if(n <= 2) {
@@ -28,6 +46,11 @@ function fibonacci(n) {
 
 //But, this is a very inelegant solution, since the fibonacci function
 //is called thousands of times.
+/**
+ * for calling fibonacci function--limits input to 25 or less, prints each iteration
+ * @param num
+ * @returns {string}
+ */
 function writeFib(num) {
   if(num > 25) return "too large";
   //Iterate over loop a
@@ -39,3 +62,37 @@ function writeFib(num) {
 }
 
 writeFib(20);
+
+/**
+ * Recursive function to make factorials
+ *
+ * @param num
+ */
+function factorial(num) {
+  if(num > 1) {
+    wr(`${num} plus`);
+    return num * factorial(num-1)
+  }
+  else {
+    return num
+  }
+}
+
+factorialFive = factorial(5);
+
+wr(`Factorial 5: ${factorialFive}`);
+
+
+
+/**
+ * Jasmin Tests
+ */
+describe('fibonacci', function() {
+  it('should add from 1 n times according to the fibonacci sequence', () => {
+    expect(fibonacci(2)).toEqual(1);
+    expect(fibonacci(3)).toEqual(2);
+    expect(fibonacci(4)).toEqual(3);
+    expect(fibonacci(5)).toEqual(5);
+    expect(fibonacci(10)).toEqual(55);
+  });
+});
