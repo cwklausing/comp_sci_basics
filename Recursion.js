952134b158afc65a2fdd248056f8eb350849af22
cwklausing <cwklausing@gmail.com>
2016-08-19 13:08:14 -0500

    complete bubblesort function :smile:

--- Recursion.js
+++ Recursion.js
@@ -89,10 +89,26 @@ wr(`Factorial 5: ${factorialFive}`);
 //Because there's an outer loop and an inner loop, the big O is n^2
 const bubbleSort = (nums) => {
   let swapped = false;
+  do {
+    swapped = false;
+    for (let i = 0; i < nums.length; i++) {
+      console.log(nums);
+      //if the element is larger than the element after it...
+      if (nums[i] > nums[i+1]) {
+        //swap the two values in the array
+        const temp = nums[i];
+        nums[i] = nums[i+1];
+        nums[i+1] = temp;
+        swapped = true
+      }
+    }
+  }
   while(swapped) {
 
   }
 }
+
+//Run bubbleSort!
 
 
 /**
@@ -117,11 +133,10 @@ describe('factorial', () => {
   });
 });
 
-xdescribe('bubble sort', () => {
+describe('bubble sort', () => {
   it('should sort an array correctly', () => {
     var nums = [10,5,3,8,2,6,4,7,9,1];
     bubbleSort(nums);
     expect(nums).toEqual([1,2,3,4,5,6,7,8,9,10]);
-    done();
   })
 })
