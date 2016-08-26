7ab84eef3e6b71c80c95ecaa3e749e25f493031e
cwklausing <cwklausing@gmail.com>
2016-08-26 14:00:54 -0500

    notes on data structure interfaces

--- Data_structure_interfaces.js
+++ Data_structure_interfaces.js
@@ -1,3 +1,37 @@
 /**
  * Created by cwklausing on 8/26/16.
  */
+/*Data structure interfaces:
+ A data structure interface is one in which consumer of data doesn't know how it works
+ underneath--it's a black box, basically.
+ */
+
+/*
+ Sets: A set is something that you can add, remove, contains, and toList.
+ sets are also useful since you can only add something to a set once--de-duplication.
+ Included in ES6
+ */
+
+/*
+  Maps: (aka dictionaries) - Similar to javascript objects. A set/collection of keys
+  that have values associated with those keys. However, unlike js objects, no prototypes,
+  inheritance, or methods.
+
+  Basically just key/value pairs.
+
+  Do not necessarily have order--bad for ordering things.
+ */
+
+/*
+  Stacks: last in, first out. For instance, if your stack is [1] and you push 2 onto it,
+  now you can no longer get at 1 until you take 2 off the stack.
+
+  We program on a stack--you can only do one thing at a time, pushing functions onto the
+  stack, doing them, popping them off the stack. Or adding more things to the stack.
+ */
+
+/*
+  Queue: First in, first out. For ex, a printer. You can also do priority queues, where
+  things have different ratings and get pushed to different spots in the queue.
+ */
+
