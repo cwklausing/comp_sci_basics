da77376d31d84fc66bf4f4ad8897fe2d8257ff9c
cwklausing <cwklausing@gmail.com>
2016-09-07 16:33:37 -0500

    Set up binary search tree challenge

--- Data_structure_interfaces.js
+++ Data_structure_interfaces.js
@@ -192,6 +192,17 @@ class Node {
  *   4          15
  */
 
+/**
+* Class Tree and Node for Binary search Tree
+*/
+ class Tree {
+
+ }
+
+ class Node {
+
+ }
+
 
 
 
@@ -199,7 +210,7 @@ class Node {
  * Jasmine Tests
  */
 
-xdescribe('ArrayList', function() {
+describe('ArrayList', function() {
     const range = length => Array.apply(null, {length: length}).map(Number.call, Number);
     const abcRange = length => range(length).map( num => String.fromCharCode( 97 + num ) );
     let list;
@@ -302,5 +313,43 @@ describe('LinkedList', function() {
         expect(list.length).toEqual(24);
         expect(list.get(0)).toEqual('b');
     });
+});
 
+describe('Binary Search Tree', function() {
+  it('creates a correct tree', () => {
+    const nums = [3,7,4,6,5,1,10,2,9,8];
+    const tree = new Tree();
+    nums.map( num => tree.add(num));
+    const objs = tree.toObject();
+    render(objs, nums);
+
+    expect(objs.value).toEqual(3);
+
+    expect(objs.left.value).toEqual(1);
+    expect(objs.left.left).toBeNull();
+
+    expect(objs.left.right.value).toEqual(2);
+    expect(objs.left.right.left).toBeNull();
+    expect(objs.left.right.right).toBeNull();
+
+    expect(objs.right.value).toEqual(7);
+
+    expect(objs.right.left.value).toEqual(4);
+    expect(objs.right.left.left).toBeNull();
+
+    expect(objs.right.left.right.value).toEqual(6);
+    expect(objs.right.left.right.left.value).toEqual(5);
+    expect(objs.right.left.right.left.right).toBeNull();
+    expect(objs.right.left.right.left.left).toBeNull();
+
+    expect(objs.right.right.value).toEqual(10);
+    expect(objs.right.right.right).toBeNull();
+
+    expect(objs.right.right.left.value).toEqual(9);
+    expect(objs.right.right.left.right).toBeNull();
+
+    expect(objs.right.right.left.left.value).toEqual(8);
+    expect(objs.right.right.left.left.right).toBeNull();
+    expect(objs.right.right.left.left.left).toBeNull();
+  });
 });
